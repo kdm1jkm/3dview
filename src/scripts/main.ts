a8b7dad172ed0085384dd3a7f8fc1ea63b3eb0b6
kdm <kdm1jkmm@gmail.com>
2021-06-16 17:18:42 +0900

    Set canvas with pointer lock

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,5 +1,27 @@
+import { fabric } from "fabric";
+
 export default class Main {
+  private readonly canvas = new fabric.StaticCanvas("canvas", {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+  private readonly canvasElement = this.canvas.getElement();
+
   constructor() {
-    console.log("Hello, World!");
+    window.addEventListener("resize", (_) => {
+      this.canvas.setWidth(window.innerWidth);
+      this.canvas.setHeight(window.innerHeight);
+    });
+
+    this.canvasElement.addEventListener("mousedown", () => {
+      console.log("Request PointerLock");
+      this.canvasElement.requestPointerLock();
+    });
+
+    this.canvasElement.addEventListener("mousemove", (e) => {
+      console.log(e.movementX, e.movementY);
+    });
+
+    this.canvas.backgroundColor = "";
   }
 }
