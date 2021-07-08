2be1128f49719c8e1d532adbc61f986c460f0247
kdm <kdm1jkmm@gmail.com>
2021-07-08 15:59:41 +0900

    Polygon Class Overrides draw function

--- src/scripts/polygon.ts
+++ src/scripts/polygon.ts
@@ -1,8 +1,24 @@
+import Drawable from "./drawable";
+
 export interface Coord {
   x: number;
   y: number;
 }
 
-export default class Polygon {
-  constructor(private readonly points: Array<Coord>) {}
+export default class Polygon implements Drawable {
+  constructor(
+    public points: Array<Coord>,
+    public point: Coord,
+    public color: string
+  ) {}
+
+  draw(context: CanvasRenderingContext2D): void {
+    context.beginPath();
+    context.moveTo(this.point.x, this.point.y);
+    this.points.forEach((p) => {
+      context.lineTo(this.point.x + p.x, this.point.y + p.y);
+    });
+    context.fillStyle = this.color;
+    context.fill();
+  }
 }
